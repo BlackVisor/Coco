19053cb337db67c220311ff32b0af3cf4f96da76
BlackVisor <812264061@gmail.com>
2018-07-11 17:43:22 +0800

    7-11

--- APITest/test.py
+++ APITest/test.py
@@ -1,29 +1,12 @@
-from Common import queryString
-import requests, time
+import re
 
-
-for j in range(1003011, 1003030):
-    content1 = {
-        'appType': 'I',
-        'languagePack': 'EN',
-        'packageName': 'com.Ejetsolutions.offerplus',
-        'sign': '',
-        'source': '0',
-        'timestamp': 1000*int(time.time()),
-        'version': '1.1.6',
-    }
-
-    content1['loginName'] = j
-    content1['password'] = 'f492a324fbf16d306ee09f5d0ac5e1eb'
-    content1['serverArea'] = 'de'
-    content1['timeZone'] = '+8'
-    content1['osVersion'] = '11.4'
-    content1['imei'] = ''
-    content1['pushType'] = 1
-    content1['ipAddress'] = '192.168.1.160'
-    content1['mac'] = ''
-    content1['electricQty'] = 58
-    content1['connectType'] = ''
-
-    b = requests.post('http://hzdev.offerplus.com:82/offerplus/login.do', data=content1)
-    print(b.text)
+with open(r"C:\Users\Oujia\Documents\Tencent Files\812264061\FileRecv\new 3.txt", "r", encoding="utf-8") as f:
+    a = f.readlines()
+pattern = r'productPrice":"\d+\.\d+"'
+c = a[0]
+b = re.findall(pattern, c)
+pattern1 = r'\d+\.\d+'
+for i,j in enumerate(b):
+    d = re.findall(pattern1, j)
+    if float(d[0]) > 0.01 or float(d[0]) < 99999.9999:
+        print(d[0])
