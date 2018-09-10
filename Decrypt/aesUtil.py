63268ec7165e1e2cbbf771ffb9bd2a55934741fd
BlackVisor <812264061@qq.com>
2018-09-10 19:24:14 +0800

    9-10 只改了pycrypto的python3.5版本的安装路径

--- Decrypt/aesUtil.py
+++ Decrypt/aesUtil.py
@@ -5,7 +5,7 @@ import gzip
 import re
 import json
 import urllib.parse
-# pip install --use-wheel --no-index --find-links=https://github.com/sfbahr/PyCrypto-Wheels/raw/master/pycrypto-2.6.1-cp37-none-win_amd64.whl pycrypto  
+# pip install --use-wheel --no-index --find-links=https://github.com/sfbahr/PyCrypto-Wheels/raw/master/pycrypto-2.6.1-cp35-none-win_amd64.whl pycrypto  
 
 
 class AppOutDecrypt:
